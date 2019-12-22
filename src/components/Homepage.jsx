f6b242bfff60f5e328d1dc48491b5fa20179bcb8
olalekanteeblaze <olalekanteeblaze@gmail.com>
2019-12-22 02:14:42 -0800

    home page and navbar

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/homepage.css'
+function Homepage() {
+    return(
+        <div className="home">
+            <div className="container">
+                <h1>Hello, I'm Moshood Abidemi.</h1>
+                <h1>I'm a fullstack developer</h1>
+                <button>View my work</button>
+            </div>
+        </div>
+    )
+}
+export default Homepage
---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+function Menu() {
+    return(
+        <div>
+            <button className="hamburger hamburger--collapse is-active" type="button">
+                <span className="hamburger-box">
+                    <span className="hamburger-inner"></span>
+                </span>
+            </button>
+        </div>
+    )
+}
+export default Menu
